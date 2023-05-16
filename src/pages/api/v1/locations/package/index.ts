4d81785cebc8233442c260f71dd59f40a24166e1
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-05-16 15:22:54 -0400

    refactor /api/v1/locations/get-kit to /package
    Signed-off-by: Pete Pongpeauk <kurtsiberg@gmail.com>

--- src/pages/api/v1/locations/package/index.ts
+++ src/pages/api/v1/locations/package/index.ts
@@ -5,10 +5,17 @@ import { tokenToID } from "@/pages/api/firebase";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  if (req.method !== "GET") {
+    res.status(405).json({ success: false, error: "Method not allowed" });
+    return;
+  }
   // request parameters & headers
   const { location_id } = req.query as { location_id: string };
   const { authorization } = req.headers as { authorization: string };
 
-  // w.i.p.
-  res.status(200).json({ success: true });
+  // return sample file
+  res.setHeader("Content-Disposition", "attachment; filename=sample.zip");
+  res.setHeader("Content-Type", "application/zip");
+  res.status(200).send("sample_file.txt");
+
 };
